c44e2861762a8efec7062e77069e0229bc509c4c
Mirai Hattori <m-hattori@jsk.t.u-tokyo.ac.jp>
2019-01-11 13:49:32 +0900

    modify t parameter

--- calc_bspline.py
+++ calc_bspline.py
@@ -18,16 +18,27 @@ def calc_bspline_base(x, j, N, _n, M, x_max, x_min):
                (xj_n_1 - x) * calc_bspline_base(x, j + 1, N, _n - 1, M, x_max, x_min)) / (_n * h)
 
 
-N = 3
+N = 5
 M = 14
 x_max = 1
 x_min = 0
 
 h = (x_max - x_min) * 1.0 / (M - N)
-c = [1.0] * M
+c = []
+for i in range(M):
+    c.append(i * 1.0 + 1)
+
+t_used = []
+t_unused = []
+
+for i in range(0, M + N + 1):
+    if N <= i and i <= M:
+        t_used.append(h * i + (M * x_min - N * x_max) / (M - N))
+    else:
+        t_unused.append(h * i + (M * x_min - N * x_max) / (M - N))
 
 fig, ax = plt.subplots()
-xx = np.linspace(x_min - h * 2, x_max + h * 2, 999)
+xx = np.linspace(h * 0 + (M * x_min - N * x_max) / (M - N), h * (M + N) + (M * x_min - N * x_max) / (M - N), 999)
 
 for i in range(M):
     ax.plot(xx, [calc_bspline(x, i, N, M, x_max, x_min) * c[i] for x in xx], '-', lw=2, label='bspline-base-'+str(i))
@@ -36,9 +47,13 @@ sum_bsp = []
 for x in xx:
     s = 0.0
     for i in range(M):
-        s += calc_bspline(x, i, N, M, x_max, x_min)
+        s += c[i] * calc_bspline(x, i, N, M, x_max, x_min)
     sum_bsp.append(s)
-ax.plot(xx, sum_bsp, '-', lw=2, label='bspline-base-'+str(i))
+ax.plot(xx, sum_bsp, '-', lw=2, label='bspline-sum')
+ax.vlines(t_used, 0, max(c), "red", linestyles='dashed')
+ax.vlines(t_unused, 0, max(c), "blue", linestyles='dashed')
+for i in range(M):
+    ax.plot(h * (i + i + N + 1) / 2.0 + (M * x_min - N * x_max) / (M - N), c[i], '.')
 #ax.plot(x_min - h / 2.0, c[0], '.')
 #ax.plot(x_min + 1.0 * h / 2.0, c[1], '.')
 #ax.plot(x_min + 3.0 * h / 2.0, c[2], '.')
